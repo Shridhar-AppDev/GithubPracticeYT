94611b2604c9d8d68406d767c69a9d24df16d119
Shridhar Chouksey <shridharchouksey100@gmail.com>
2022-01-06 01:37:20 +0530

    Change 2 Commit

--- app/src/main/java/com/app/githubpracticeyt/MainActivity.kt
+++ app/src/main/java/com/app/githubpracticeyt/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("This is my another branch")
+        println("Change 2")
     }
 }
